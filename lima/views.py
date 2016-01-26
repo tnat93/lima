f4d4907b87bc863dc16f9b0c29d4ee93cf21a3d8
tnat93 <xoxo_tammy@hotmail.com>
2016-01-26 16:46:29 -0600

    File additions to project
    Added the following files to the project: urls.py, views.py,
    navbar-static-top.css, about.html, base.html, home.html,
    javascript.html, navbar.html.
    Additions within the files are currently commented.
    Will edit later.

--- lima/views.py
+++ lima/views.py
@@ -2,6 +2,9 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic.base import TemplateView
 from django.utils.decorators import method_decorator
 
+# from django.shortcuts import render
+
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -9,3 +12,6 @@ class IndexView(TemplateView):
     @method_decorator(ensure_csrf_cookie)
     def dispatch(self, *args, **kwargs):
         return super(IndexView, self).dispatch(*args, **kwargs)
+
+# def about(request):
+#     return render(request, "about.html", {})
