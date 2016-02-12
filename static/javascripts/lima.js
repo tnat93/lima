e8581aee513d3ca3aaa560522da2326a00fe1d6e
tnat93 <xoxo_tammy@hotmail.com>
2016-02-12 17:31:18 -0600

    Pushing changes

--- static/javascripts/lima.js
+++ static/javascripts/lima.js
@@ -1,2 +1,12 @@
-angular
-  .module('lima', []);
+(function(angular){
+  'use strict';
+  angular
+    .module('lima', []);
+    .controller('searchCtrl', ['$scope', function($scope){
+      $scope.keyword = "Testing";
+
+      $scope.display = function() {
+        $scope.result = $scope.keyword + "1, 2, 3";
+      };
+    }]);
+});
